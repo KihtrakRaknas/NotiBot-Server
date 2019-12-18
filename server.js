436b8bad230722bf5004e865b719ca0559d6cee8
KihtrakRaknas <Karthikandsankar@gmail.com>
2019-12-18 12:24:27 -0500

    added express

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 var admin = require('firebase-admin');
+const express = require('express');
+const PORT = process.env.PORT || 5000
 var serviceAccount;
 
 if(process.env.firebaseKey)
     serviceAccount = JSON.parse(process.env.firebaseKey)
 else
     serviceAccount = require("./secureContent/serviceAccountKey.json");
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://notibotapp.firebaseio.com"
-  });
+});
 
-  let db = admin.firestore();
+let db = admin.firestore();
+
+var app=express();
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+});
 
   db.collection("cities").doc("SF").get().then(function(doc) {
     if (doc.exists) {
